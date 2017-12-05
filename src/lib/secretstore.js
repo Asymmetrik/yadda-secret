422be2c1ea1b101b1f5664b35046223fbae22d14
Jason Young <jyoung@asymmetrik.com>
2017-12-05 16:35:44 -0500

    Using console.error instead of console.log.

--- src/lib/secretstore.js
+++ src/lib/secretstore.js
@@ -24,7 +24,7 @@ class SecretStore {
         if(cacheBuster) {
             const cacheBusterKey = secretGen(cacheBuster);
             setInterval(() => {
-                this.store.getSecret({name: cacheBusterKey}).then(secret => this.cacheRefreshTime = Number(secret)).catch(console.log);
+                this.store.getSecret({name: cacheBusterKey}).then(secret => this.cacheRefreshTime = Number(secret)).catch(console.error);
             }, 60000);
         }
     }
