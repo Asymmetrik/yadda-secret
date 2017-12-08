f86a95b6975756443cc50542fa4102e5fcc8b350
Jason Young <jyoung@asymmetrik.com>
2017-12-08 10:44:30 -0500

    Escaping the key name.

--- src/lib/secretstore.js
+++ src/lib/secretstore.js
@@ -25,7 +25,13 @@ class SecretStore {
                 TableName: options.table,
                 Limit: 1,
                 ConsistentRead: true,
-                KeyConditionExpression: 'name = ' + secretGen(options.cacheBuster)
+                KeyConditionExpression: '#name = :name',
+                ExpressionAttributeNames: {
+                    '#name': 'name'
+                },
+                ExpressionAttributeValues: {
+                    ':name': secretGen(options.cacheBuster)
+                }
             }
             setInterval(() => {
                 console.log(this.cacheRefreshTime);
